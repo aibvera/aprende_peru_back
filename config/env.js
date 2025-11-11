255a2b26f0bc43e4708376264dba00d6ce70a6ef
Alejandro <alejandro.bueno.vera.97@gmail.com>
2025-11-10 21:01:36 -0500

    aws env

--- config/env.js
+++ config/env.js
@@ -8,11 +8,11 @@ export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(",") || [];
 export const DEBUG = process.env.DEBUG === "true";
 
 // BD
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PORT = process.env.DB_PORT;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_NAME = process.env.DB_NAME;
+export const DB_HOST = process.env.AWS_DB_HOST;
+export const DB_PORT = process.env.AWS_DB_PORT;
+export const DB_USER = process.env.AWS_DB_USER;
+export const DB_PASSWORD = process.env.AWS_DB_PASSWORD;
+export const DB_NAME = process.env.AWS_DB_NAME;
 
 // JWT
 export const JWT_SECRET = process.env.JWT_SECRET;
